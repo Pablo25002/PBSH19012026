f965d7f4ddc3ae56d60740edab1181f3d07de462
Pablo25002 <sh25002@esfe.agape.edu.sv>
2026-01-19 14:57:31 -0600

    Conecta rutas de autenticación

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const connectDB = require('./src/config/database');
+const authRoutes = require('./src/routes/authRoutes');
+
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('API funcionando');
