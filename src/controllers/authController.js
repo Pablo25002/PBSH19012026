b2329ea86da038b6bf4d16c53713e4b37af7beb1
Pablo25002 <sh25002@esfe.agape.edu.sv>
2026-01-19 14:54:54 -0600

    Controlador de autenticación

---
+++ src/controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: 'Usuario creado correctamente' });
+  } catch (error) {
+    res.status(400).json({ message: 'Error al crear usuario' });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: 'Credenciales incorrectas' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Credenciales incorrectas' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      'secretkey',
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Error en login' });
+  }
+};
