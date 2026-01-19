c486bbe7f26bd037e518d524d1a3c616833c73c7
Pablo25002 <sh25002@esfe.agape.edu.sv>
2026-01-19 15:20:02 -0600

    Crea modelo Clinte

---
+++ src/models/client.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const clientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  dui: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Client', clientSchema);
