dc6b6e88911fd7f281073a7609cb192a4c0185b5
Pablo25002 <sh25002@esfe.agape.edu.sv>
2026-01-19 16:18:09 -0600

    ruta de búsqueda de clientes

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -4,6 +4,7 @@ const clientController = require('../controllers/clientController');
 const { verifyToken } = require('../controllers/authController');
 
 router.post('/', verifyToken, clientController.createClient);
+router.get('/search', verifyToken, clientController.searchClient);
 router.get('/', verifyToken, clientController.getClients);
 router.get('/:id', verifyToken, clientController.getClientById);
 router.put('/:id', verifyToken, clientController.updateClient);
