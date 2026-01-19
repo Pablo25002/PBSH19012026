8412c8e70b582815ebc51ae6a0419794e2774134
Pablo25002 <sh25002@esfe.agape.edu.sv>
2026-01-19 16:15:05 -0600

    búsqueda de clientes por DUI o nombre

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -29,3 +29,26 @@ exports.deleteClient = async (req, res) => {
   await Client.findByIdAndDelete(req.params.id);
   res.json({ message: 'Cliente eliminado' });
 };
+exports.searchClient = async (req, res) => {
+  const { dui, name } = req.query;
+
+  try {
+    let clients;
+
+    if (dui) {
+      clients = await Client.find({ dui });
+    } else if (name) {
+      clients = await Client.find({
+        name: { $regex: name, $options: 'i' }
+      });
+    } else {
+      return res.status(400).json({
+        message: 'Debe enviar dui o name para buscar'
+      });
+    }
+
+    res.json(clients);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al buscar cliente' });
+  }
+};
